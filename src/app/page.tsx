b7a15cd2dcf858b28556423094fcfc1432b38a54
Developer <developer@innovativemojo.com>
2025-02-17 14:26:50 +0500

    navbar added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
-import Button from "@/components/ui/Button";
-import Text from "@/components/ui/Text";
+import HeroSection from "@/components/Home/HeroSection";
+
 
 export default function Home() {
   return (
     <>
-      <div className="min-h-screen bg-black">
-        <Text>SHOP</Text>
-        <Text as="h2">LIMITED EDITION PRINTS</Text>
-        <Text as="h1">LOREM IPSUM</Text>
-        <Button >LIMITED EDITION</Button>
-      </div>
+    
+    <HeroSection/>
     </>
   );
 }
