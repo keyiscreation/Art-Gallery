9302f5a8eb643937ac7726b6d8a04deed06d54e0
salmanamjad123 <103861241+salmanamjad123@users.noreply.github.com>
2025-02-19 19:52:07 +0500

    Update index.tsx

--- src/components/AboutMe/index.tsx
+++ src/components/AboutMe/index.tsx
@@ -10,6 +10,7 @@ import youtube from "@/public/icons/youtube.svg";
 import twiter from "@/public/icons/u_twitter.svg";
 import insta from "@/public/icons/u_instagram.svg";
 import fb from "@/public/icons/u_facebook-f.svg";
+
 const AboutMe = () => {
   return (
     <div>
