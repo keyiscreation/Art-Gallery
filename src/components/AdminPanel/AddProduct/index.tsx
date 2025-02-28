b13734513c0a8d88bdfa2c522b7fdca39b1b58e6
Faheem <faheem@studio93.io>
2025-02-28 15:41:44 +0500

    firebase setup is done

---
+++ src/components/AdminPanel/AddProduct/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { db } from "@/firebase";
+import { collection, addDoc } from "firebase/firestore";
+
+const AddProduct = () => {
+  return <div>AddProduct</div>;
+};
+
+export default AddProduct;
