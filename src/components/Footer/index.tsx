e25c9cbe8d6f3d2ec6ef015e6170d3b73610f694
salmanamjad123 <103861241+salmanamjad123@users.noreply.github.com>
2025-04-14 11:16:25 +0500

    Update index.tsx

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -10,7 +10,6 @@ import twiter from "@/public/icons/u_twitter.svg";
 import insta from "@/public/icons/u_instagram.svg";
 import fb from "@/public/icons/u_facebook-f.svg";
 
-
 const Footer = () => {
   const [email, setEmail] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(false);
