9852428e7ef12560c7b1a724c70ad80ec2c02a0e
salman amjad <salmanamjad902@gmail.com>
2026-03-03 23:43:28 +0500

    svg adjusted in footer

--- src/components/Footer/FooterStore.tsx
+++ src/components/Footer/FooterStore.tsx
@@ -110,7 +110,7 @@ const Footer = () => {
 
         <Link className="text-[#F4C430] text-sm flex items-center gap-1" href="https://www.instagram.com/keyiscreation?igsh=MWhzcHJqeWN1N292dQ==">
           {/* <Image className="w-[24.52px] h-[24.52px]" src={insta} alt="insta" /> */}
-          <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" data-fg-b7rw67=":914.573:/App.tsx:893:17:30165:598:e:svg:etete"><rect x="2" y="2" width="20" height="20" rx="5" ry="5" data-fg-b7rw68=":914.573:/App.tsx:904:19:30541:56:e:rect"></rect><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" data-fg-b7rw69=":914.573:/App.tsx:905:19:30616:59:e:path"></path><line x1="17.5" y1="6.5" x2="17.51" y2="6.5" data-fg-b7rw70=":914.573:/App.tsx:906:19:30694:46:e:line"></line></svg>
+          <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" data-fg-b7rw67=":914.573:/App.tsx:893:17:30165:598:e:svg:etete"><rect x="2" y="2" width="20" height="20" rx="5" ry="5" data-fg-b7rw68=":914.573:/App.tsx:904:19:30541:56:e:rect"></rect><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" data-fg-b7rw69=":914.573:/App.tsx:905:19:30616:59:e:path"></path><line x1="17.5" y1="6.5" x2="17.51" y2="6.5" data-fg-b7rw70=":914.573:/App.tsx:906:19:30694:46:e:line"></line></svg>
            @keyiscreation
         </Link>
         {/* <Image className="w-[24.52px] h-[24.52px]" src={fb} alt="fb" /> */}
